2f5c68d5876f534462b5f02f0048c03af7d63b5e
Gerhard Bijker <gerhard.bijker@ihsmarkit.com>
2022-06-25 20:00:43 +0200

    Eerste probeerslag van afkapping - alle unieke woorde in die Psalmboek

--- hypernator.R
+++ hypernator.R
@@ -1,9 +1,20 @@
 # install.packages("hyphenatr")
-devtools::install_github("hrbrmstr/hyphenatr")
+# devtools::install_github("hrbrmstr/hyphenatr")
 library(hyphenatr)
 
 # hyphenatr::list_dicts()
 hyphenatr::switch_dict("af_ZA")
 
-dat <- readLines(system.file("extdata/top10000en.txt", package="hyphenatr"))
-microbenchmark(out1 <- hyphenate(dat))
+load("verse.Rdata")
+woorde <- unique(tolower(unname(unlist(strsplit(unlist(Ps), " ")))))
+woorde <- gsub(",|\\.|\\?|;|!|:|\"|\\(|\\)|/", "", woorde)  # strip leestekens / punctuation
+woorde <- gsub("^[0-9]*[\\.]*$", "", woorde)   # remove verse indent
+woorde <- unique(woorde[woorde!=""])
+
+afkap <- gsub("=", "-", hyphenate(woorde, TRUE))
+Encoding(afkap) <- "UTF-8"
+
+altwee <- as.data.frame(list(woord=woorde, afkap=afkap))
+write.table(altwee[order(altwee$woord), ], "afkap.txt", quote = FALSE, row.names = FALSE)
+
+
